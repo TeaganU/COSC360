64099c3a5d252d7a1c0027e682053d997b696f38
teagan <turquhar@student.ubc.ca>
2026-03-05 17:42:05 -0800

    Added guest/user view of navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,42 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Searchbar from "./Searchbar";
 
 export default function Navbar() {
+  const isLoggedIn = false; // placeholder
   return (
-    <nav className="bg-white border-b  px-8 py-4 flex justify-between items-center">
-      <div className="font-bold text-lg flex gap-6">
+    <nav className="bg-white border-b  px-8 py-4 flex justify-between items-center ">
+      <div className="font-bold text-lg flex gap-6 items-center">
         Outdoor Skill Sharing
         <Searchbar />
       </div>
 
-      <div className="flex gap-6">
+      <div className="flex gap-6 items-center">
         <Link to="/">Home</Link>
         <Link to="/skills">Skills</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup" className="bg-black text-white px-4 py-1 rounded">
-          Sign Up
-        </Link>
+        
+        {isLoggedIn ? (
+          <Link
+            to="/profile"
+            className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-black text-white"
+          >
+            JD
+          </Link>
+        ) : (
+          <>
+            <Link 
+              to="/login"
+            >
+              Login
+            </Link>
+
+            <Link 
+              to="/signup" 
+              className="bg-black text-white px-4 py-1 rounded"
+            >
+              Sign Up
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
